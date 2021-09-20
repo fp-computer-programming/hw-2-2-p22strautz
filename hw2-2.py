0e90dc8a9812fc5a3722f6d1d61c8f78306eb877
p22strautz <p22strautz@fairfieldprep.org>
2021-09-20 10:20:41 -0400

    hw2-2

---
+++ hw2-2.py
@@ -0,0 +1,11 @@
+# Author: SCT (ADMG) 9/20/21
+
+free_throws = 2
+two_pointers = 2
+three_pointers = 2
+
+score = (free_throws) + (two_pointers * 2) + (three_pointers * 3)
+print(score)
+
+print("The player scored ## points in the game.")
+
